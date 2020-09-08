7e1b037347d9c9b40ec8a047da81355b7aacb19f
Prasad Medarametla <medarametla.l.v.prasad@gmail.com>
2020-09-08 12:09:35 +0530

    Conditional dom inclusion using terinary operator

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import React, { Component } from 'react';
      persons: [
        {name: 'Prasad',age: 30},
        {name: 'Pragna',age: 32},
-     ]
+     ],
+     showPersons: false
    }
 
    switchNameHandler = () => {
@@ -25,6 +26,12 @@ import React, { Component } from 'react';
     })
    }
 
+   togglerPersonsHandler = () => {
+    this.setState({
+      showPersons: !this.state.showPersons
+    })
+   }
+
    render() {
      const buttonStyle = {
        backgroundColor: 'white',
@@ -35,19 +42,24 @@ import React, { Component } from 'react';
      }
      return (
        <div className="App">
-         <h1>Hi this is react demo</h1>
-         <button 
-          onClick={this.switchNameHandler}
-          style={buttonStyle}>Switch Name</button>
-         <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}
-          click={this.switchNameHandler}
-          changed={this.changeNameFirstRowHandler}>My Hobbies: Jogging</Person>
-         <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}/>
-       </div>
+        <h1>Hi this is react demo</h1>
+        <button 
+          onClick={this.togglerPersonsHandler}
+          style={buttonStyle}>Toggle Persons
+        </button>
+        {this.state.showPersons ? 
+          <div>
+            <Person 
+              name={this.state.persons[0].name} 
+              age={this.state.persons[0].age}
+              click={this.switchNameHandler}
+              changed={this.changeNameFirstRowHandler}>My Hobbies: Jogging</Person>
+            <Person 
+              name={this.state.persons[1].name} 
+              age={this.state.persons[1].age}/>
+          </div>
+       : null}
+      </div>
      );
      //return React.createElement('div',{className: 'App'},React.createElement('h1',null,'This is react App!!'));
    }
