ca1081cbe184159a093f1bcc6fa8ceda861fb411
Prasad Medarametla <medarametla.l.v.prasad@gmail.com>
2020-09-08 05:30:06 +0530

    Done wt 3: Understanding d base features & syntax

--- src/App.js
+++ src/App.js
@@ -1,33 +1,56 @@
-import React, { useState } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from 'react';
+ import './App.css';
+ import Person from './Person/Person';
 
-const app = () => {
+ class App extends Component {
 
-  const [personsState,setPersonsState] = useState({
-    persons: [
-      {name: 'Prasad',age: 30},
-      {name: 'Pragna',age: 32},
-    ]
-  });
+   state = {
+     persons: [
+       {name: 'Prasad',age: 30},
+       {name: 'Pragna',age: 32},
+     ]
+   }
 
-  const switchNameHandler = () => {
-    setPersonsState({
-      persons: [
-        {name: 'Pragna',age: 32},
-        {name: 'Prasad',age: 30},
-      ]
+   switchNameHandler = () => {
+     this.setState({
+       persons: [...this.state.persons.reverse()]
+     })
+   }
+
+   changeNameFirstRowHandler = (event) => {
+    let rows = [...this.state.persons]
+    rows[0].name = event.target.value
+    this.setState({
+      persons: [...rows]
     })
-  }
+   }
 
-  return (
-    <div className="App">
-      <h1>Hi this is react demo</h1>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>My Hobbies: Jogging</Person>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
-    </div>
-  );
-}
+   render() {
+     const buttonStyle = {
+       backgroundColor: 'white',
+       font: 'inherit',
+       border: '1px solid blue',
+       padding: '8px',
+       cursor: 'pointer'
+     }
+     return (
+       <div className="App">
+         <h1>Hi this is react demo</h1>
+         <button 
+          onClick={this.switchNameHandler}
+          style={buttonStyle}>Switch Name</button>
+         <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler}
+          changed={this.changeNameFirstRowHandler}>My Hobbies: Jogging</Person>
+         <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}/>
+       </div>
+     );
+     //return React.createElement('div',{className: 'App'},React.createElement('h1',null,'This is react App!!'));
+   }
+ }
 
-export default app;
+ export default App;
